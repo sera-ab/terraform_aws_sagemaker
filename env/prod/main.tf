7af455595aeb439521ef8ad36ea95351a0288299
Sera Aboagye <51058242+sera-ab@users.noreply.github.com>
2022-05-23 19:47:09 -0500

    prod main.tf

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,1 +1,17 @@
-h
+locals {
+    config = yamldecode(file("../../config.yml"))["users"]
+    users = flatten([for user in local.config:
+    {
+      "name" = user.name
+      "size" = user.size
+    }
+    if contains(user.env, "prod")
+    ])
+}
+
+module "sagemaker" {
+  for_each = {for user in local.users : user.name => user }
+  source        = "../../sagemaker"
+  sm_name       = each.value.name
+  instance_type = each.value.size
+}
