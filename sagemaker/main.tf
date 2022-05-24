b98428b40a73af4a366b43324b74f98d68c226d0
Sera Aboagye <51058242+sera-ab@users.noreply.github.com>
2022-05-23 20:12:21 -0500

    sagemaker main file

--- sagemaker/main.tf
+++ sagemaker/main.tf
@@ -1,1 +1,34 @@
-i
+resource "aws_sagemaker_notebook_instance" "uat_sm" {
+  name                    = var.sm_name
+  role_arn                = aws_iam_role.notebook_iam_role.arn
+  instance_type           = var.instance_type
+
+  tags = {
+    Name = var.sm_name
+  }
+}
+
+resource "aws_iam_policy_attachment" "sm_full_access_attach" {
+  name = "${var.sm_name}-policy"
+  roles = [aws_iam_role.notebook_iam_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+resource "aws_iam_role" "notebook_iam_role" {
+  name = "${var.sm_name}-role"
+  path = "/"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : "sagemaker.amazonaws.com"
+          },
+          "Effect" : "Allow"
+        }
+      ]
+    }
+  )
+}
