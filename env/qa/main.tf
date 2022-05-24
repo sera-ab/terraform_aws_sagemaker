2925b8567548d6aac2887d0a67e2b3773d8756eb
sera-ab <seraaboagye.it@gmail.com>
2022-05-23 19:52:43 -0500

    main file

---
+++ env/qa/main.tf
@@ -0,0 +1,17 @@
+locals {
+    config = yamldecode(file("../../config.yml"))["users"]
+    users = flatten([for user in local.config:
+    {
+      "name" = user.name
+      "size" = user.size
+    }
+    if contains(user.env, "qa")
+    ])
+}
+
+module "sagemaker" {
+  for_each = {for user in local.users : user.name => user }
+  source        = "../../sagemaker"
+  sm_name       = each.value.name
+  instance_type = each.value.size
+}
